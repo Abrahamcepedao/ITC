74fdac9ddd1f180d0f015475a81008e5b23426f2
Abrahamcepe <abrahamgolf@gmail.com>
2020-08-17 09:35:08 -0500

    Prepared order algorithms

---
+++ Semestre3/estructuras_de_datos_algoritmos_fundamentales/tareas/searchAlgorithms/README.md
@@ -0,0 +1,11 @@
+# Act 1.2 Search and order algorithms - Abraham Cepeda Oseguera - A00827666
+This activity consists of several algorithms that implements binary search algorithms and order algorithms
+
+## Complexity of each algorithm
+* Factorial   - O(n)
+* Sumas       - O(n)
+* Pyramid     - O(n)
+* Fibonacci   - O(n)
+* Bacteria    - O(n)
+* Investment  - O(n)
+* Power       - O(n)
---
+++ Semestre3/estructuras_de_datos_algoritmos_fundamentales/tareas/searchAlgorithms/orderExchange.cpp
@@ -0,0 +1,26 @@
+// This algorithm calculates de value of the number in the fibonacci sequence of any given position
+// Abraham Cepeda Oseguera
+// A00827666
+// August 17th 2020
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void orderExchange(vector<int> &numbers){
+
+}
+
+int main(){
+    int n, num;
+    cout << "Enter the n value: ";
+    cin >> n;
+    vector<int> numbers;
+    for(int i = 0; i < n; i++){
+        cout << "Enter a number: ";
+        cin >> num;
+        numbers.push_back(num);
+    }
+    orderExchange(numbers);
+    return 0;
+}
