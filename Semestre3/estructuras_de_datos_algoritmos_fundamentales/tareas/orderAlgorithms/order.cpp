4540f4c951ea140dede59e14d6e922e9c61aa1c9
Abrahamcepe <abrahamgolf@gmail.com>
2020-08-20 11:01:32 -0500

    Added order by Selection algorithm

--- Semestre3/estructuras_de_datos_algoritmos_fundamentales/tareas/orderAlgorithms/order.cpp
+++ Semestre3/estructuras_de_datos_algoritmos_fundamentales/tareas/orderAlgorithms/order.cpp
@@ -18,10 +18,12 @@ void exchange(vector<T> &vect, int num1, int num2){
 }
 
 template<class T>
-void orderExchange(vector<T> &vect){
+void orderExchange(vector<T> &vect, int &comp, int &inter){
     for(int i = 0; i < vect.size(); i++){
         for(int j = i+1; j < vect.size(); j++){
+            comp++;
             if(vect[i] > vect[j]){
+                inter++;
                 exchange(vect, i, j);
             }
         }
@@ -29,10 +31,12 @@ void orderExchange(vector<T> &vect){
 }
 
 template<class T>
-void orderBubble(vector<T> &vect){
+void orderBubble(vector<T> &vect, int &comp, int &inter){
     for(int i = 0; i < vect.size(); i++){
         for(int j = 0; j < (vect.size()-i); j++){
+            comp++;
             if(vect[j] > vect[j+1]){
+                inter++;
                 exchange(vect, j, j+1);
             }
         }
@@ -40,10 +44,31 @@ void orderBubble(vector<T> &vect){
 }
 
 template<class T>
-void printVector(vector<T> vect){
+void orderSelection(vector<T> &vect, int &comp, int &inter){
+    T min;
     for(int i = 0; i < vect.size(); i++){
-        cout << vect[i] << "\n";
+        min = i;
+        for(int j = i+1; j < vect.size(); j++){
+            comp++;
+            if(vect[j] < vect[min]){
+                min = j;
+            }
+        }
+        if(min != i){
+            inter++;
+            exchange(vect, i, min);
+        }
     }
+}
+
+
+template<class T>
+void printVector(vector<T> vect, int comp, int inter){
+    for(int i = 0; i < vect.size(); i++){
+        cout << vect[i] << " ";
+    }
+    cout << "\nComparisons: " << comp << "\n";
+    cout << "Exchanges:   " << inter << "\n";
 }
 
 template<class T>
@@ -56,9 +81,9 @@ void createVector(vector<T> &vect, int n, int max){
 
 int menu(){
     int ans;
-    cout << "1. Order by Exchange\n";
+    cout << "\n\n\n\n1. Order by Exchange\n";
     cout << "2. Order by Bubble\n";
-    cout << "3. Order by Merge\n";
+    cout << "3. Order by Selection\n";
     cout << "0. To exit\n\n";
     cout << "Enter answer: ";
     cin >> ans;
@@ -72,25 +97,31 @@ int main(){
     while(ans != 0){
         vector<int> vect;
         createVector(vect, n, 100);
+        int comp = 0, inter = 0;
         ans = menu();
+        printVector(vect, comp, inter);
         switch (ans){
             case 1:
-                cout << "Ordered vector by Exchange\n";
-                orderExchange(vect);
+                cout << "\nOrdered vector by Exchange\n";
+                orderExchange(vect, comp, inter);
                 break;
             case 2:
-                cout << "Ordered vector by Bubble\n";
-                orderExchange(vect);
+                cout << "\nOrdered vector by Bubble\n";
+                orderExchange(vect, comp, inter);
                 break;
             case 3:
-                cout << "Ordered vector by Merge\n";
+                cout << "\nOrdered vector by Direct Selection\n";
+                orderSelection(vect, comp, inter);
+                break;
+            case 4:
+                cout << "\nOrdered vector by Merge\n";
                 //orderExchange(vect);
                 break;
             default:
                 break;
         }
         if(ans != 0){
-            printVector(vect);
+            printVector(vect, comp, inter);
         } else{
             cout << "The program has finished\n";
         }
