f2f70c4106d35023c60d375394e9b56e10f1af1a
Abrahamcepe <abrahamgolf@gmail.com>
2020-08-27 09:36:10 -0500

    Added functionality to binarySearch

--- Semestre3/estructuras_de_datos_algoritmos_fundamentales/tareas/searchAlgorithms/binarySearch.cpp
+++ Semestre3/estructuras_de_datos_algoritmos_fundamentales/tareas/searchAlgorithms/binarySearch.cpp
@@ -8,7 +8,18 @@
 using namespace std;
 
 int binarySearch(vector<int> numbers, int num){
-    
+    int low = 0;
+    int high = numbers.size()-1;
+    while(low <= high){
+        int medium = (high + low) / 2;
+        if(numbers[medium] == num){
+            return medium;
+        } else if(numbers[medium] > num){
+            high = medium - 1;
+        } else{
+            low = medium + 1;
+        }
+    }
     return -1;
 }
 
@@ -17,8 +28,8 @@ int main(){
     cout << "Enter the n value: ";
     cin >> n;
     vector<int> numbers;
-    for (int i = 0; i < n; i++){
-        numbers.push_back(i+1);
+    for (int i = 1; i <= n; i++){
+        numbers.push_back(i);
     }
     cout << "Enter the value you want to look for: ";
     cin >> num;
