bf598a3e45a9b770cb390ce83a6b1b844e3d5f20
Abrahamcepe <abrahamgolf@gmail.com>
2020-09-07 17:24:44 -0500

    Finished readData function

--- Semestre3/estructuras_de_datos_algoritmos_fundamentales/ TC1031-A00827666/Act1.3/main.cpp
+++ Semestre3/estructuras_de_datos_algoritmos_fundamentales/ TC1031-A00827666/Act1.3/main.cpp
@@ -22,10 +22,9 @@ struct Registry{
     string err;
 };
 
-void split(vector<string> vect, string str, string pattern){
+void split(vector<string> &vect, string str, string pattern){
     int beg = 0, end = 0;
     string value;
-    cout << "size " << str.size() << endl;
     while(end >= 0){
         end = str.find(pattern, beg);
         value = str.substr(beg, end - beg);
@@ -35,15 +34,38 @@ void split(vector<string> vect, string str, string pattern){
     
 }
 
+void printRegistry(Registry registry){
+    cout << "\n" << registry.mon << "  " << registry.day << "  " << registry.hour << ":" << registry.min << ":" << registry.sec << "  " << registry.ip << "  " << registry.err;
+}
+
+void getErrorData(string &str, vector<string> values){
+    str = values[4];
+    for(int i = 5; i < values.size(); i++){
+        str = str + " " + values[i];
+    }
+}
+
 void readData(vector<Registry> &registries){
     //open file
     ifstream file("bitacora.txt");
     vector<string> values;
+    vector<string> date;
     string line;
     int count = 0;
     while(getline(file, line) && count < 10){
+        Registry registry;
         split(values, line, " ");
-        
+        registry.mon = values[0];
+        registry.day = stoi(values[1]);
+        split(date, values[2], ":");
+        registry.hour = stoi(date[0]);
+        registry.min = stoi(date[1]);
+        registry.sec = stoi(date[2]);
+        registry.ip = values[3];
+        getErrorData(registry.err, values);
+        registries.push_back(registry);
+        values.clear();
+        date.clear();
         count++;
     }
 }
@@ -52,5 +74,7 @@ int main(){
     // create vector
     vector<Registry> registries;
     readData(registries);
+    printRegistry(registries[0]);
+    printRegistry(registries[5]);
     return 0;
 }
