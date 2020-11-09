a7a066d2541e346d4707f702963c40b5b23c22ae
Abrahamcepe <abrahamgolf@gmail.com>
2020-11-09 15:54:14 -0600

    Modified graphs

--- Semestre3/estructuras_de_datos_algoritmos_fundamentales/clases/Graphs/main.cpp
+++ Semestre3/estructuras_de_datos_algoritmos_fundamentales/clases/Graphs/main.cpp
@@ -1,7 +1,7 @@
 #include  "Graph.h"
 #include <vector>
 int main(){
-    vector<vector<char>> list{{'a', 'b'}, {'a', 'c'}, {'a', 'e'}, {'b', 'a'}, {'b', 'd'}, {'c', 'a'}, {'c', 'd'}, {'d', 'b'}, {'d', 'c'}, {'d', 'e'}, {'e', 'a'}, {'e', 'd'}};
+    vector< vector<char> > list = {{'a', 'b'}, {'a', 'c'}, {'a', 'e'}, {'b', 'a'}, {'b', 'd'}, {'c', 'a'}, {'c', 'd'}, {'d', 'b'}, {'d', 'c'}, {'d', 'e'}, {'e', 'a'}, {'e', 'd'}};
     /* list.push_back({'a', 'b'});
     list.push_back({'a', 'c'});
     list.push_back({'a', 'e'});
